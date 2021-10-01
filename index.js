8edcf64a167fec90021a1081845e4e437ee2eda6
David Pacheco <pachecomorahectordavid@gmail.com>
2021-10-01 17:30:05 -0500

    Se cambia parte de la estructura HTML y se comienza con proyecto mono clase

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+
 const ejecutar = () =>{
     terminar = true
     let opc = prompt("Buenos dias, por fabor indique si es un empleado o es un cliente \n porfavor indiquelo con la palabra exacta")
+/*
+
     while(terminar == true){
         if(opc == empleado){
-
-        }else if(opc == cliente ){
-            
+            menu.show_menu()
+        }else if(opc == cliente ){   
         }
-
+        else{
+            console.log("Opcion invalida ingresela nuevamente")
+        }
     }
+
+*/
+menu.show_menu()
 }
